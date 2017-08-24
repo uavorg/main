a50f809a9972b1d5c22eba13163c1ce416a86fad
lbay <boanyeliu@creditease.cn>
2017-08-24 19:48:25 +0800

    修改架构、菜单调整、用户调整、底部调整

--- index/assets/js/core.js
+++ index/assets/js/core.js
@@ -47,16 +47,7 @@ $(document).ready(function () {
     if ($.fn.revolution) {
         revapi = $('#home').revolution(
 	{
-	    delay: 15000,
-	    startwidth: 1170,
-	    startheight: 500,
-	    hideThumbs: 10,
-	    fullWidth: "off",
-	    fullScreen: "on",
-	    navigationType: "none",
-	    fullScreenOffsetContainer: "",
 	    touchenabled: "on",
-	    videoJsPath: "assets/plugins/rs-plugin/videojs/"
 	});
 
     }
