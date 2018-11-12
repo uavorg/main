e02f4a4a05598e8605d99aecba27e084e432a5b7
jiayusong2 <jiayusong2@creditease.cn>
2018-11-12 16:25:55 +0800

    bug fix

--- weeklyUpdate.js
+++ weeklyUpdate.js
@@ -1,10 +1,14 @@
 var updateData = {
-  href: "https://github.com/uavorg/uavstack/releases/tag/UAVStack_1.2_20181026",
+  href: "https://github.com/uavorg/uavstack/releases/tag/UAVStack_1.2_20181109",
   update: [
-    "1. 简化了预警触发策略中梯度收敛规则的相关配置，如需每次触发则直接填写-1。",
-    "2. 优化了预警记录在Redis中的存储结构。",
-    "3. 新增了根据预警记录的'已处理'状态决定报警是否持续的功能。",
-    "4. MOF支持了对HttpServletResponse的多层套壳（Wrapper）的处理能力。"
+    "1.  新增对Spring  MVC的RequestMapping注解画像支持。",
+    {
+      title: "2.  增强MSCP的功能: ",
+      tips: [
+        "a.  当MSCP启动端口被占用时自动退出。",
+        "b.  当Http调用无匹配处理器时，返回404。"
+      ]
+    }
   ],
-  fix: ["1.  修复了广播地址在某些场景下获取失败导致的NPE问题。"]
+  fix: ["1.  FIX  修复监控代理程序在某些情况下启动失败的问题。 "]
 };
