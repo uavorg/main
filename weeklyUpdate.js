f1b9676cff4d199a27df6481f0419a79fa8bbbfe
jiayusong <jiayusong2@creditease.cn>
2018-11-16 18:23:21 +0800

    增加图片链接+解除ant css引用

--- weeklyUpdate.js
+++ weeklyUpdate.js
@@ -1,9 +1,8 @@
 var updateData = {
   href: "https://github.com/uavorg/uavstack/releases/tag/UAVStack_1.2_20181116",
-  update: [
-    "1. 新增 Apphub用户登录支持系统用户认证方式与LDAP认证方式并存。"
-  ],
-  fix: ["1.  FIX  修复监控代理程序在某些情况下启动失败的问题。",
+  update: ["1. 新增 Apphub用户登录支持系统用户认证方式与LDAP认证方式并存。"],
+  fix: [
+    "1.  FIX  修复监控代理程序在某些情况下启动失败的问题。",
     "2. FIX 修复了Tomcat启动时MOF导致的FileNotFoundException问题。",
     "3. FIX 修复了在某些情况下重调用链NPE问题。"
   ]
