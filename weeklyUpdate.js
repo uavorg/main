2015b0991c8fc2faecf68a471c43c2384fa6f79e
jiayusong <jiayusong2@creditease.cn>
2018-11-16 17:51:46 +0800

    周更

--- weeklyUpdate.js
+++ weeklyUpdate.js
@@ -1,14 +1,10 @@
 var updateData = {
-  href: "https://github.com/uavorg/uavstack/releases/tag/UAVStack_1.2_20181109",
+  href: "https://github.com/uavorg/uavstack/releases/tag/UAVStack_1.2_20181116",
   update: [
-    "1.  新增对Spring  MVC的RequestMapping注解画像支持。",
-    {
-      title: "2.  增强MSCP的功能: ",
-      tips: [
-        "a.  当MSCP启动端口被占用时自动退出。",
-        "b.  当Http调用无匹配处理器时，返回404。"
-      ]
-    }
+    "1. 新增 Apphub用户登录支持系统用户认证方式与LDAP认证方式并存。"
   ],
-  fix: ["1.  FIX  修复监控代理程序在某些情况下启动失败的问题。 "]
+  fix: ["1.  FIX  修复监控代理程序在某些情况下启动失败的问题。",
+    "2. FIX 修复了Tomcat启动时MOF导致的FileNotFoundException问题。",
+    "3. FIX 修复了在某些情况下重调用链NPE问题。"
+  ]
 };
