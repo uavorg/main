2e40cf7b87b46a2668a453f4876e8eb1a559c823
[jiayusong2] <[jiayusong2@creditease.cn]>
2018-12-14 19:50:10 +0800

    weekly update

--- weeklyUpdate.js
+++ weeklyUpdate.js
@@ -1,8 +1,5 @@
 var updateData = {
     href: "https://github.com/uavorg/uavstack/releases/tag/UAVStack_1.2_20181207",
-    update: ["1.增强对Spring MVC的RequestMapping注解画像支持。",
-        "2.新增重调用链对PrintWriter的支持。",
-        "3.新增调用链对ResponseWrapper的支持。"
-    ]
-
+    update: [],
+	fix:["1:FIX 修复了helper.js 拷贝函数在某些情况下拷贝失败问题。"]
 };
