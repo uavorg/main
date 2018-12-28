975ce18c513069b7b6dea4d24b64be7a28b91b96
[jiayusong2] <[jiayusong2@creditease.cn]>
2018-12-28 19:04:30 +0800

    Weekly Update

---
+++ update.bat
@@ -0,0 +1,3 @@
+git add .
+git commit -m "Weekly Update"
+git push origin master
--- weeklyUpdate.js
+++ weeklyUpdate.js
@@ -1,5 +1,7 @@
 var updateData = {
-    href: "https://github.com/uavorg/uavstack/releases/tag/UAVStack_1.2_20181221",
-    update: ["1.新增 HealthManager可通过Lettuce实现对Redis集群的支持"],
-	fix:["1.FIX 修复了进程监控数据保存到OpenTSDB时个别Tag为空的问题"]
+    href: "https://github.com/uavorg/uavstack/releases/tag/UAVStack_1.2_20181228",
+    update: ["1. 新增 MSCP程序启动时日志网卡列表信息增加“网卡名”字段","2. 新增 预警功能增加对MA进程死亡的支持"],
+	fix:["1. FIX 修复同环比预警阈值下限某些情况下计算错误的问题"
+	,"2. FIX 修复预警策略某些情况下在内存中未删除的问题"
+	,"3. FIX 修复日志关键字搜索某些情况下显示不正确的问题 "]
 };
