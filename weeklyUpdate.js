7a2e84574478d3709bee0ec7e268e6c6f8800da4
jihaiying <892425470@qq.com>
2019-01-11 18:45:08 +0800

    Weekly Update

--- weeklyUpdate.js
+++ weeklyUpdate.js
@@ -1,7 +1,5 @@
 var updateData = {
-    href: "https://github.com/uavorg/uavstack/releases/tag/UAVStack_1.2_20181228",
-    update: ["1. 新增 MSCP程序启动时日志网卡列表信息增加“网卡名”字段","2. 新增 预警功能增加对MA进程死亡的支持"],
-	fix:["1. FIX 修复同环比预警阈值下限某些情况下计算错误的问题"
-	,"2. FIX 修复预警策略某些情况下在内存中未删除的问题"
-	,"3. FIX 修复日志关键字搜索某些情况下显示不正确的问题 "]
+    href: "https://github.com/uavorg/uavstack/releases/tag/UAVStack_1.2_20190111",
+    update: [],
+	fix:["1. FIX 修复某些情况下MA无法正确加载tools.jar的问题"]
 };
