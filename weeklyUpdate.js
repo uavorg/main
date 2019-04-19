9bdb178e6e3c530935dba16c54096e2e47c52ae2
[jiayusong2] <[jiayusong2@creditease.cn]>
2019-04-19 18:17:15 +0800

    Weekly Update

--- weeklyUpdate.js
+++ weeklyUpdate.js
@@ -1,5 +1,6 @@
 var updateData = {
-    href: "https://github.com/uavorg/uavstack/releases/tag/UAVStack_1.2_20190111",
-    update: [],
-	fix:["1. FIX 修复某些情况下MA无法正确加载tools.jar的问题"]
+    href: "https://github.com/uavorg/uavstack/releases/tag/UAVStack_1.3_20190419",
+    update: ["1. 新增 调用链对于用户自定义异步场景的支持",
+	"如：A系统调用B系统，A系统接收请求之后立刻返回给B系统一个状态信息，然后启用一个或多个线程池继续处理的情况。"],
+	fix:[""]
 };
